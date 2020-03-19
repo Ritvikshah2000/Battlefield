16f70cb415f3a2d2ed7f1092439eaaf6204b5148
mslavnic <milansslavnic@gmail.com>
2020-03-18 20:21:46 -0700

    Revert "Pushing final code, along with .JAR file"
    This reverts commit c2e98fbc2244c2ced6653b569ff6273586fcdcf4.

---
+++ BATTLEFIELD/src/main/java/hazard/Bomb.java
@@ -0,0 +1,22 @@
+package hazard;
+
+import java.awt.*;
+
+
+public abstract class Bomb {
+
+    int x, y, index;
+
+    Bomb(int x, int y) {
+        this.x =x;
+        this.y =y;
+    }
+
+    public int getX(){ return x / 32; };
+    public int getY(){ return y / 32; };
+
+    public abstract void onHit();
+
+    public abstract void update(Graphics2D g);
+
+}
