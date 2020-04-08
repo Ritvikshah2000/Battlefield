316ab8f727d34c15a259bf8156f654e6f8965bed
mslavnic <milansslavnic@gmail.com>
2020-04-08 14:19:51 -0700

    Working on Issue #31 Added Javadocs to KeyReward.java

--- src/main/java/reward/KeyReward.java
+++ src/main/java/reward/KeyReward.java
@@ -13,11 +13,21 @@ import java.awt.Graphics2D;
 
 public final class KeyReward extends Reward {
 
+    /**
+     * Initializes this KeyReward instance at position x, y
+     * @param x x position real pixel value as an int
+     * @param y y position real pixel value as an int
+     */
     public KeyReward(final int x, final int y) {
         super(x * Tile.TILESIZE, y *Tile.TILESIZE);
         setValue(100);
     }
 
+    /**
+     * Increases the Player keyCount, increases Score and removes this instance from the game
+     * @see Player
+     * @see Score
+     */
     public void onHit() {
         KeySound keysound = new KeySound();
         Player.IncreaseKeyCount();
@@ -28,6 +38,10 @@ public final class KeyReward extends Reward {
         KeyBar.getKbar().add(new KeyReward(KeyBar.getInitialTile() + KeyBar.increaseCollected(), 0));
     }
 
+    /**
+     * Redraws this KeyRewards graphics
+     * @param g Graphics2D instance used to draw graphics
+     */
     public void update(final Graphics2D g) {
         g.drawImage(Images.getKeysImage(), getX(), getY(), Tile.TILESIZE, Tile.TILESIZE, null);
     }
