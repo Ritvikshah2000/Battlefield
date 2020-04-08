ffa31ba7cc895c2f1f3dd73b43fc03ce28687d23
mslavnic <milansslavnic@gmail.com>
2020-04-08 11:12:28 -0700

    Working on Issue #22 KeySound.java Code Style Added Javadocs to KeySound.java

--- src/main/java/music/KeySound.java
+++ src/main/java/music/KeySound.java
@@ -1,12 +1,23 @@
 package music;
 
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.Clip;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class KeySound {
+public final class KeySound {
     private Clip keysound;
 
+    /**
+     * Laods the audio clip to be played when the Player collects a KeyReward
+     *
+     * @see javax.sound.sampled
+     * @see actor.Player
+     * @see reward.KeyReward
+     */
     public KeySound()
     {
         try
@@ -25,6 +36,13 @@ public class KeySound {
         }
     }
 
+    /**
+     * Terminates the audio clip that plays when the Player collects a KeyReward
+     *
+     * @see javax.sound.sampled.Clip
+     * @see actor.Player
+     * @see reward.KeyReward
+     */
     public void stop()
     {
         this.keysound.close();
