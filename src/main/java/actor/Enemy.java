e4f1124712abfbad72e9e9ed2fbb3d7f5d854d5e
mslavnic <milansslavnic@gmail.com>
2020-04-08 00:12:00 -0700

    Working on Issue #14 Formatted code style

--- src/main/java/actor/Enemy.java
+++ src/main/java/actor/Enemy.java
@@ -3,6 +3,7 @@ package actor;
 import board.TestLevel;
 import board.Tile;
 import image.Images;
+
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
@@ -25,8 +26,9 @@ public final class Enemy {
 
     /**
      * Initializes Enemy instance at positions x, y, with sprite corresponding to imageId
-     * @param x x position real pixel value
-     * @param y y position real pixel value
+     *
+     * @param x       x position real pixel value
+     * @param y       y position real pixel value
      * @param imageId ID of which sprite to load from Images
      * @see Images
      */
@@ -44,114 +46,125 @@ public final class Enemy {
 
     /**
      * Returns x position real pixel value
+     *
      * @return x position real pixel value
      */
-    public int getX(){
+    public int getX() {
         return x;
     }
 
     /**
      * Returns y position real pixel value
+     *
      * @return y position real pixel value
      */
-    public int getY(){
+    public int getY() {
         return y;
     }
 
     /**
      * Returns whether this Enemy can move up on the board
+     *
      * @return boolean value determining this Enemy's ability to move up
      */
-    public boolean getCanUp(){
+    public boolean getCanUp() {
         return canUp;
     }
+
     /**
      * Returns whether this Enemy can move down on the board
+     *
      * @return boolean value determining this Enemy's ability to move down
      */
-    public boolean getCanDown(){
+    public boolean getCanDown() {
         return canDown;
     }
 
     /**
      * Returns whether this Enemy can move left on the board
+     *
      * @return boolean value determining this Enemy's ability to move left
      */
-    public boolean getCanLeft(){
+    public boolean getCanLeft() {
         return canLeft;
     }
 
     /**
      * Returns whether this Enemy can move right on the board
+     *
      * @return boolean value determining this Enemy's ability to move right
      */
-    public boolean getCanRight(){
+    public boolean getCanRight() {
         return canRight;
     }
 
     /**
      * Returns amount of damage enemies deals as a float
+     *
      * @return amount of damage enemies deals as a float
      */
-    public static float getDamage(){
+    public static float getDamage() {
         return damage;
     }
 
     /**
      * Returns amount of time this Enemy will be frozen for as an int
+     *
      * @return amount of time this Enemy will be frozen for as an int
      */
-    public int getFrozenTimer(){
+    public int getFrozenTimer() {
         return frozenTimer;
     }
 
     /**
      * Sets the amount of time this Enemy will be frozen for
+     *
      * @param time integer value for amount of time this Enemy will be frozen for
      */
-    public void setFrozenTimer(int time){
+    public void setFrozenTimer(int time) {
         frozenTimer = time;
     }
 
     /**
      * Returns whether or not Enemies are frozen
+     *
      * @return boolean value isFrozen
      */
-    public static boolean getIsFrozen(){
+    public static boolean getIsFrozen() {
         return isFrozen;
     }
 
     /**
      * Returns this Enemy's bounding Rectangle
+     *
      * @return this Enemy's bounding Rectangle
      * @see Rectangle
      */
-    public Rectangle getBounds(){
+    public Rectangle getBounds() {
         return new Rectangle(x, y, SIZE, SIZE);
     }
 
     /**
      * Checks whtether or not enemies are frozen and sets them accordingly
      */
-    public void checkFrozen()
-    {
-        if(frozenTimer != 0){
+    public void checkFrozen() {
+        if (frozenTimer != 0) {
             isFrozen = true;
             frozenTimer -= 1;
-        }else{
+        } else {
             isFrozen = false;
         }
     }
 
     /**
      * Moves this Enemy towards the Player and redraws it's graphics accordingly
+     *
      * @param g Graphics2D instance used to redraw graphics
      */
-    public void chasingHero(Graphics2D g)
-    {
-        if(!isFrozen){  // chasing hero
+    public void chasingHero(Graphics2D g) {
+        if (!isFrozen) {  // chasing hero
             if (g != null)
-                switch(enemyImageId){
+                switch (enemyImageId) {
                     case 1:
                         g.drawImage(Images.getEnemyImage(), x, y, SIZE, SIZE, null);
                         break;
@@ -162,40 +175,37 @@ public final class Enemy {
                         g.drawImage(Images.getEnemy3Image(), x, y, SIZE, SIZE, null);
                         break;
                 }
-            if(Player.x > this.x && canRight) {
+            if (Player.x > this.x && canRight) {
                 x += speed;
             }
-            if(Player.y > this.y && canDown) {
+            if (Player.y > this.y && canDown) {
                 y += speed;
             }
-            if(Player.x < this.x && canLeft) {
+            if (Player.x < this.x && canLeft) {
                 x -= speed;
             }
-            if(Player.y < this.y && canUp) {
+            if (Player.y < this.y && canUp) {
                 y -= speed;
             }
-        }
-        else
-        {
+        } else {
             if (g != null)
-                g.drawImage(Images.getEnemyFrozenImage(), (int)x, (int)y, SIZE, SIZE, null);
+                g.drawImage(Images.getEnemyFrozenImage(), (int) x, (int) y, SIZE, SIZE, null);
         }
     }
 
     /**
      * Checks if this Enemy has collided with a barrier and stops it's movement accordingly
      */
-    public void collision(){
+    public void collision() {
 
         Rectangle enemyrect = this.getBounds();
         Rectangle barrierrect = null;
 
         // check can left
         canLeft = true;
-        for (int i=0; i<TestLevel.barrierList.size(); i++)
-        {
+        for (int i = 0; i < TestLevel.barrierList.size(); i++) {
             Tile barrier = TestLevel.barrierList.get(i);
-            barrierrect = new Rectangle(barrier.x+1, barrier.y, barrier.width, barrier.height);
+            barrierrect = new Rectangle(barrier.x + 1, barrier.y, barrier.width, barrier.height);
             if (enemyrect.intersects(barrierrect)) {
                 canLeft = false;
                 break;
@@ -204,10 +214,9 @@ public final class Enemy {
 
         // check can right
         canRight = true;
-        for (int i=0; i<TestLevel.barrierList.size(); i++)
-        {
+        for (int i = 0; i < TestLevel.barrierList.size(); i++) {
             Tile barrier = TestLevel.barrierList.get(i);
-            barrierrect = new Rectangle(barrier.x-1, barrier.y, barrier.width, barrier.height);
+            barrierrect = new Rectangle(barrier.x - 1, barrier.y, barrier.width, barrier.height);
             if (enemyrect.intersects(barrierrect)) {
                 canRight = false;
                 break;
@@ -216,10 +225,9 @@ public final class Enemy {
 
         // check can up
         canUp = true;
-        for (int i=0; i<TestLevel.barrierList.size(); i++)
-        {
+        for (int i = 0; i < TestLevel.barrierList.size(); i++) {
             Tile barrier = TestLevel.barrierList.get(i);
-            barrierrect = new Rectangle(barrier.x, barrier.y+1, barrier.width, barrier.height);
+            barrierrect = new Rectangle(barrier.x, barrier.y + 1, barrier.width, barrier.height);
             if (enemyrect.intersects(barrierrect)) {
                 canUp = false;
                 break;
@@ -228,10 +236,9 @@ public final class Enemy {
 
         // check can down
         canDown = true;
-        for (int i=0; i<TestLevel.barrierList.size(); i++)
-        {
+        for (int i = 0; i < TestLevel.barrierList.size(); i++) {
             Tile barrier = TestLevel.barrierList.get(i);
-            barrierrect = new Rectangle(barrier.x, barrier.y-1, barrier.width, barrier.height);
+            barrierrect = new Rectangle(barrier.x, barrier.y - 1, barrier.width, barrier.height);
             if (enemyrect.intersects(barrierrect)) {
                 canDown = false;
                 break;
@@ -241,6 +248,7 @@ public final class Enemy {
 
     /**
      * Composite function which checks for collision(), checkFrozen() and calls chasingHero(Graohics2D)
+     *
      * @param g Graphics2D instance used to redraw graphics
      */
     public void update(final Graphics2D g) {
