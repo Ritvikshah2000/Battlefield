ce0dfb496833e58edb9508fcb5d24f165d9a6a68
mslavnic <milansslavnic@gmail.com>
2020-04-08 13:56:09 -0700

    Working on Issue #29 Added Javadoc to Freeze.java

--- src/main/java/reward/Freeze.java
+++ src/main/java/reward/Freeze.java
@@ -14,12 +14,24 @@ public final class Freeze extends Reward
     private int freezeTime = 180;
     private int timeLeft = 750;
 
+    /**
+     * Initializes this Freeze instance at position x, y
+     * @param x x index of Tile occupied by this Freeze reward
+     * @param y y index of Tile occupied by this Freeze reward
+     * @see Tile
+     * @see TestLevel
+     */
     public Freeze(final int x, final int y)
     {
         super(x * Tile.TILESIZE, y * Tile.TILESIZE);
         value = 150;
     }
 
+    /**
+     * Freezes all instances of Enemy on the map and removes this Freeze reward
+     *
+     * @see actor.Enemy
+     */
     public void onHit(){
         FreezeSound freezeSound = new FreezeSound();
         Main.getEnemy().forEach((e) -> e.setFrozenTimer(freezeTime));
@@ -29,6 +41,10 @@ public final class Freeze extends Reward
         Main.getRewards().remove(this);
     }
 
+    /**
+     * Redraws this Freeze rewards graphics while it is available on the map
+     * @param g Graphics2D instance used to redraw graphics
+     */
     public void update(final Graphics2D g){
         if(timeLeft > 0){
             g.drawImage(Images.getFreezeImage(), getX(), getY(), Tile.TILESIZE, Tile.TILESIZE, null);
