cb3389b43d9aa21987988e236dc5d65c93e21d91
mslavnic <milansslavnic@gmail.com>
2020-04-08 00:55:11 -0700

    Working on Issue #16 Added Javadocs to KeyBar.java

--- src/main/java/actor/KeyBar.java
+++ src/main/java/actor/KeyBar.java
@@ -10,14 +10,28 @@ public class KeyBar{
     private static ArrayList<KeyReward> kBar = new ArrayList<KeyReward>();
     private static int collected = 0;
 
+    /**
+     * Returns list of collected KeyReward instances
+     * @return an ArrayList of collected KeyReward instances
+     * @see KeyReward
+     */
     public static ArrayList<KeyReward> getKbar() { return kBar; }
 
+    /**
+     * Increment the number of KeyRewards collected
+     * @return an int value of KeyRewards collected
+     */
     public static int increaseCollected() {
         collected++;
         return collected;
     }
 
-    public static void update(Graphics2D g){
+    /**
+     * Redraws the KeyBar graphics
+     * @param g Graphics2D instance used to redraw graphics
+     * @see Graphics2D
+     */
+    public static void update(final Graphics2D g){
         kBar.forEach((k) -> k.update(g));
     }
 }
