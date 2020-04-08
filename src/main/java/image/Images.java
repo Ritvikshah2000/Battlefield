213318e384fdea4aee3c5c9859381d6399df346d
mslavnic <milansslavnic@gmail.com>
2020-04-07 21:09:20 -0700

    Added Javadocs to Images.java

--- src/main/java/image/Images.java
+++ src/main/java/image/Images.java
@@ -53,23 +53,102 @@ public class Images {
         }
     }
 
+    /**
+     * Loads all image resources
+     */
     public static void loadImages(){
        new Images();
     }
 
-
+    /**
+     * Returns Enemy image resource file as a BufferedImage
+     * @return Enemy image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getEnemyImage() { return Enemy; }
+
+    /**
+     * Returns first alternate Enemy image resource file as a BufferedImage
+     * @return first alternate Enemy image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getEnemy2Image() { return Enemy2; }
+
+    /**
+     * Returns second alternate Enemy image resource file as a BufferedImage
+     * @return second alternate Enemy image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getEnemy3Image() { return Enemy3; }
+
+    /**
+     * Returns Player image resource file as a BufferedImage
+     * @return Player image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getPlayerImage() { return Player; }
+
+    /**
+     * Returns HealthBomb image resource file as a BufferedImage
+     * @return HealthBomb image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getHealthBombImage() { return HealthBomb; }
+
+    /**
+     * Returns ScoreBomb image resource file as a BufferedImage
+     * @return ScoreBomb image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getScoreBombImage() { return ScoreBomb; }
+
+    /**
+     * Returns Freeze image resource file as a BufferedImage
+     * @return Freeze image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getFreezeImage() { return Freeze; }
+
+    /**
+     * Returns grass image resource file as a BufferedImage
+     * @return grass image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getGrassImage() { return Grass; }
+
+    /**
+     * Returns wall image resource file as a BufferedImage
+     * @return wall image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getWallImage() { return Wall; }
+
+    /**
+     * Returns EndPoint image resource file as a BufferedImage
+     * @return EndPoint image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getEndPointImage() { return EndPoint; }
+
+    /**
+     * Returns HealthReward image resource file as a BufferedImage
+     * @return HealthReward image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getHeartsImage() { return Hearts; }
+
+    /**
+     * Returns KeyReward image resource file as a BufferedImage
+     * @return KeyReward image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getKeysImage() { return Keys; }
+
+    /**
+     * Returns FrozenEnemy image resource file as a BufferedImage
+     * @return FrozenEnemy image resource as a BufferedImage
+     * @see BufferedImage
+     */
     public static BufferedImage getEnemyFrozenImage() { return EnemyFrozen; }
 
 }
