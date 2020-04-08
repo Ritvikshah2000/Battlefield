b61f624c140077ee4100ed6a6e39ead3788316cd
mslavnic <milansslavnic@gmail.com>
2020-04-08 10:48:40 -0700

    Working on Issue #17 Code Style Added Javadocs to Winning.java

--- src/main/java/music/Winning.java
+++ src/main/java/music/Winning.java
@@ -1,21 +1,29 @@
 package music;
 
-import javax.sound.sampled.*;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.Clip;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Winning {
-    private Clip WinningSound;
+public final class Winning {
+    private Clip winningSound;
 
+    /**
+     * Loads the audio clip to be played when the Player wins the game
+     * @see javax.sound.sampled
+     */
     public Winning()
     {
         try
         {
-            this.WinningSound = AudioSystem.getClip();
+            this.winningSound = AudioSystem.getClip();
             InputStream is = Winning.class.getClassLoader().getResourceAsStream("sound/wingame.wav");
             AudioInputStream ais = AudioSystem.getAudioInputStream(is);
-            this.WinningSound.open(ais);
-            this.WinningSound.start();
+            this.winningSound.open(ais);
+            this.winningSound.start();
         } catch (LineUnavailableException e) {
             e.printStackTrace();
         } catch (UnsupportedAudioFileException e) {
@@ -25,8 +33,11 @@ public class Winning {
         }
     }
 
-    public void stop()
-    {
-        this.WinningSound.close();
+    /**
+     * Terminates the audio clipped played when the Player wins the game
+     * @see javax.sound.sampled.Clip
+     */
+    public void stop() {
+        this.winningSound.close();
     }
 }
