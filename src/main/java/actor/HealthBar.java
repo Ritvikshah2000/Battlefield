724e675da169e93c3556cfd9f2848c328856dcb8
mslavnic <milansslavnic@gmail.com>
2020-04-07 22:40:28 -0700

    Closing Issue $11 HealthBar Code Style and Javadoc

--- src/main/java/actor/HealthBar.java
+++ src/main/java/actor/HealthBar.java
@@ -11,15 +11,33 @@ public final class HealthBar{
     private int x;
     private int y;
 
+    /**
+     * Initializes a HealthBar instance at position x, y
+     * @param x x position real pixel value
+     * @param y y position real pixel value
+     */
     public HealthBar(final int x, final int y){
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Returns x position real pixel value
+     * @return x position real pixel value
+     */
     public int getX() { return x; }
 
+    /**
+     * Returns y position real pixel value
+     * @return y position real pixel value
+     */
     public int getY() { return y; }
 
+    /**
+     * Redraws this HealthBars graphics
+     * @param g Graphics2D used to redraw HealthBar graphics
+     * @see Graphics2D
+     */
     public void update(final Graphics2D g){
         g.drawImage(Images.getHeartsImage(), getX(), getY(), Tile.TILESIZE, Tile.TILESIZE,null);
     }
